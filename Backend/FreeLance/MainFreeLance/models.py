6a8eb526664a295911a82f24a2a2d2c88f208426
Дмитрий <kdmetrea@gmail.com>
2024-07-01 23:19:04 +0300

    modified:   Backend/FreeLance/MainFreeLance/models.py 	modified:   Backend/FreeLance/MainFreeLance/views.py 	new file:   Frontend/FreeLance/src/assets/about.txt 	new file:   Frontend/FreeLance/src/assets/android-chrome-192x192.png 	new file:   Frontend/FreeLance/src/assets/android-chrome-512x512.png 	new file:   Frontend/FreeLance/src/assets/apple-touch-icon.png 	new file:   Frontend/FreeLance/src/assets/favicon-16x16.png 	new file:   Frontend/FreeLance/src/assets/favicon-32x32.png 	new file:   Frontend/FreeLance/src/assets/favicon.ico 	new file:   "Frontend/FreeLance/src/assets/icons8-\320\277\320\276\320\273\321\214\320\267\320\276\320\262\320\260\321\202\320\265\320\273\321\214-\320\261\320\265\320\267-\320\277\320\276\320\273\320\276\320\262\321\213\321\205-\320\277\321\200\320\270\320\267\320\275\320\260\320\272\320\276\320\262-100.png" 	new file:   "Frontend/FreeLance/src/assets/icons8-\320\277\320\276\320\273\321\214\320\267\320\276\320\262\320\260\321\202\320\265\320\273\321\214-\320\261\320\265\320\267-\320\277\320\276\320\273\320\276\320\262\321\213\321\205-\320\277\321\200\320\270\320\267\320\275\320\260\320\272\320\276\320\262-50.png" 	new file:   "Frontend/FreeLance/src/assets/icons8-\321\202\320\265\321\201\321\202\320\276\320\262\321\213\320\271-\320\260\320\272\320\272\320\260\321\203\320\275\321\202-100.png" 	new file:   Frontend/FreeLance/src/assets/site.webmanifest

--- Backend/FreeLance/MainFreeLance/models.py
+++ Backend/FreeLance/MainFreeLance/models.py
@@ -1,3 +1,40 @@
 from django.db import models
+from  django.contrib.auth.models import User
 
-# Create your models here.
+class Profile(models.Model):
+    User = models.OneToOneField(User, on_delete=models.CASCADE)
+    Bio = models.TextField(max_length=500, blank=True)
+    Image = models.ImageField(upload_to='profile_pics', blank=True)
+    CreatedJobs = models.ForeignKey('Jobs', on_delete=models.CASCADE, null=True)
+    CompletedWork = models.ForeignKey('Jobs', on_delete=models.CASCADE, null=True)
+    ListChats = models.ForeignKey('Chats', on_delete=models.CASCADE, null=True)
+
+class Jobs(models.Model):
+    Creator = models.ForeignKey("Profile", on_delete=models.CASCADE)
+    Title = models.CharField(max_length=100)
+    Description  = models.TextField(max_length=500)
+    Category  = models.CharField(max_length=100)
+    File = models.FileField(upload_to='job_files')
+    Created  = models.DateTimeField(auto_now_add=True)
+    Completed= models.DateTimeField(null=True)
+    CompletedBy  = models.ForeignKey('Profile', on_delete=models.CASCADE, null=True)
+    Image = models.ImageField(upload_to='job_pics', blank=True)
+    Reward  = models.IntegerField(default=0)
+    Status   = models.CharField(max_length=100, default='Pending')
+    Chat  = models.ForeignKey('Chats', on_delete=models.CASCADE, null=True)
+
+class Chats(models.Model):
+    Job = models.ForeignKey('Jobs', on_delete=models.CASCADE, null=True)
+    ListUsers  = models.ForeignKey('Profile', on_delete=models.CASCADE)
+    Messages   = models.ForeignKey('Messages', on_delete=models.CASCADE, null=True)
+    Image  = models.ImageField(upload_to='chat_pics', blank=True)
+    Created   = models.DateTimeField(auto_now_add=True)
+    Title = models.CharField(max_length=100)
+
+class Messages(models.Model):
+    Sender  = models.ForeignKey('Profile', on_delete=models.CASCADE)
+    Chat  = models.ForeignKey('Chats', on_delete=models.CASCADE)
+    Text   = models.TextField(max_length=500)
+    Image   = models.ImageField(upload_to='chat_pics', blank=True)
+    Created   = models.DateTimeField(auto_now_add=True)
+    File    = models.FileField(upload_to='chat_files', blank=True)
